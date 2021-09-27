2c71af8207cfb2b3b87a78bcefbf097ad18c934f
Rainer Mueller <rainer.mueller@austinisd.org>
2021-09-27 17:21:48 -0500

    just using diffLoc (got rid of tkdiff and kdiff separate variables)

--- customize.py
+++ customize.py
@@ -1,14 +1,18 @@
 # java and python class periods
-validClassPeriods = ["1","4","5","6"]     
+validClassPeriods = ["1","2","5","6","8","9"]     
 
 # directories (can use either / or \)
-rootDir = r'C:\Users\d_e151509\Downloads\demo'
-scoreboardDir = r'C:\Users\d_e151509\Downloads\demo\scoreboard'
-pythonIdeLoc = r'C:/Users/d_E151509/AppData/Local/Programs/Python/Python310/Lib/idlelib/idle.pyw'
+# rootDir = r'C:\Users\e151509\Downloads\demo'
+rootDir = r'C:/Users/E151509/Dropbox/Apps/StudentFiles'
+#scoreboardDir = r'C:\Users\e151509\Downloads\demo\scoreboard'
+#scoreboardDir = r'G:\My Drive\My LASA\misc\tools\CSAssignmentChecker\scoreboards'
+scoreboardDir = r'C:/Users/E151509/Google Drive/Course Materials/Introduction to Computer Science/10.Python/scoreboard'
+pythonIdeLoc = r'C:/Users/E151509/AppData/Local/Programs/Python/Python310/Lib/idlelib/idle.pyw'
 javaIdeLoc = r'c:/Program Files (x86)/jGRASP/bin/jgrasp.exe'      
 textEditorLoc = r'c:/Program Files (x86)/Notepad++/notepad++.exe'
-tkdiffLoc = r'c:/Program Files/TkDiff/tkdiff.exe'
-kdiff3Loc = r'c:\Program Files\KDiff3\kdiff3.exe'
+#diffLoc = r'c:/Program Files/TkDiff/tkdiff.exe'
+#diffLoc = r'c:\Program Files\KDiff3\kdiff3.exe'
+diffLoc = r'C:\Program Files (x86)\Meld\Meld.exe'
 
 # email (email login is in login.py)
 emailSignature = "\n\nMr. Mueller\n\n"
