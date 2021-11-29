b68a84785fc435558c0ddf99ce24693f9db23c7e
Rainer Mueller <rainer.mueller@austinisd.org>
2021-11-29 17:21:42 -0600

    Demo

--- customize.py
+++ customize.py
@@ -1,20 +1,20 @@
-# java and python class periods
-validClassPeriods = ["1","2","5","6","8","9"]     
-
-# directories (can use either / or \)
-# rootDir = r'C:\Users\e151509\Downloads\demo'
-rootDir = r'C:/Users/E151509/Dropbox/Apps/StudentFiles'
-#scoreboardDir = r'C:\Users\e151509\Downloads\demo\scoreboard'
-#scoreboardDir = r'G:\My Drive\My LASA\misc\tools\CSAssignmentChecker\scoreboards'
-scoreboardDir = r'C:/Users/E151509/Google Drive/Course Materials/Introduction to Computer Science/10.Python/scoreboard'
+# programs
 pythonIdeLoc = r'C:/Users/E151509/AppData/Local/Programs/Python/Python310/Lib/idlelib/idle.pyw'
 javaIdeLoc = r'c:/Program Files (x86)/jGRASP/bin/jgrasp.exe'      
 textEditorLoc = r'c:/Program Files (x86)/Notepad++/notepad++.exe'
 #diffLoc = r'c:/Program Files/TkDiff/tkdiff.exe'
-#diffLoc = r'c:\Program Files\KDiff3\kdiff3.exe'
-diffLoc = r'C:\Program Files (x86)\Meld\Meld.exe'
+#diffLoc = r'c:/Program Files/KDiff3/kdiff3.exe'
+diffLoc = r'C:/Program Files (x86)/Meld/Meld.exe'
+
+
+##### For Demo
+# java and python class periods
+validClassPeriods = ["1","4","5"]
+
+# directories
+rootDir = r'C:/Users/E151509/Downloads/demo'
+scoreboardDir = r'C:/Users/E151509/Downloads/demo/scoreboard'
 
 # email (email login is in login.py)
-emailSignature = "\n\nMr. Mueller\n\n"
-emailAttachmentFile = r"C:/Users/E151509/Documents/CSassignmentChecker.jpg"  # use newest file in this dir as email attachment
-
+emailSignature = "/nYour Teacher/n"
+emailAttachmentFile = r"C:/Users/E151509/Downloads/CSassignmentChecker.jpg"
