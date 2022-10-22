bd791ef3e8e268570746c897d76723f30595d110
Rainer Mueller <rainer.mueller@austinisd.org>
2022-10-22 07:47:06 -0500

    updated

--- customize.py
+++ customize.py
@@ -1,11 +1,14 @@
 # programs
 pythonIde = r'C:/Users/E151509/AppData/Local/Programs/Python/Python310/Lib/idlelib/idle.pyw'
+#pythonIde = r'C:\Users\E151509\AppData\Local\Programs\Thonny\thonny.exe'
 javaIde = r'c:/Program Files (x86)/jGRASP/bin/jgrasp.exe'      
 textEditor = r'c:/Program Files (x86)/Notepad++/notepad++.exe'
+textEditorAlt = r'c:/Program Files/Notepad++/notepad++.exe'
 #diffPgm = r'c:/Program Files/TkDiff/tkdiff.exe'
 #diffPgm = r'c:/Program Files/KDiff3/kdiff3.exe'
 #diffPgm = r'C:/Program Files (x86)/Meld/Meld.exe'
 diffPgm = r'C:\Program Files\WinMerge/WinMergeU.exe'
+diffPgmAlt = r'C:\Program Files (x86)\WinMerge\WinMergeU.exe'
 
 # java and python class periods
 validClassPeriods = ["4","5","7","9"]
@@ -14,8 +17,8 @@ validClassPeriods = ["4","5","7","9"]
 rootDir = r'C:/Users/E151509/Dropbox/Apps/StudentFiles'
 #scoreboardDir = r'C:/Users/E151509/Google Drive/Course Materials/Introduction to Computer Science/10.Python/scoreboard'
 #scoreboardDirAlt = r'C:/Users/E151509/My Drive/Course Materials/Introduction to Computer Science/10.Python/scoreboard'
-scoreboardDir = r'C:\Users\E151509\Google Drive\My LASA\scoreboard'
-scoreboardDirAlt = r'C:\Users\E151509\My Drive\My LASA\scoreboard'
+scoreboardDir = r'C:\Users\E151509\My Drive\My LASA\scoreboard'
+scoreboardDirAlt = r'C:\Users\E151509\Google Drive\My LASA\scoreboard'
 mossDir = r'C:\Users\E151509\Google Drive\My LASA\misc\tools\moss plagiarize checking'
 
 # email (email login is in login.py)
