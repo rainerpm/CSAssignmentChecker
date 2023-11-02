7565dac7b37391fb57fa7109d3842de1bf3e3c14
Rainer Mueller <rainer.mueller@austinisd.org>
2023-11-02 11:30:17 -0500

    got rid of alternate locations (since I now run CSAC on only 1 PC and the files are always in one place)

--- customize.py
+++ customize.py
@@ -1,30 +1,24 @@
 # programs
-pythonIde = r'C:/Users/E151509/AppData/Local/Programs/Python/Python310/Lib/idlelib/idle.pyw'
-#pythonIde = r'C:\Users\E151509\AppData\Local\Programs\Thonny\thonny.exe'
+pythonIde = r'C:/Users/E151509/AppData/Local/Programs/Python/Python311/Lib/idlelib/idle.pyw'
 javaIde = r'c:/Program Files (x86)/jGRASP/bin/jgrasp.exe'      
-textEditor = r'c:/Program Files (x86)/Notepad++/notepad++.exe'
-textEditorAlt = r'c:/Program Files/Notepad++/notepad++.exe'
-#diffPgm = r'c:/Program Files/TkDiff/tkdiff.exe'
-#diffPgm = r'c:/Program Files/KDiff3/kdiff3.exe'
-#diffPgm = r'C:/Program Files (x86)/Meld/Meld.exe'
-diffPgm = r'C:\Program Files\WinMerge/WinMergeU.exe'
-diffPgmAlt = r'C:\Program Files (x86)\WinMerge\WinMergeU.exe'
+textEditor = r'c:/Program Files/Notepad++/notepad++.exe'
+diffPgm = r'C:\Program Files (x86)\WinMerge\WinMergeU.exe'
 
 # java and python class periods
-validClassPeriods = ["4","5","7","9"]
+validClassPeriods = ["1","6","7","8","9"]
 
 # directories
 rootDir = r'C:/Users/E151509/Dropbox/Apps/StudentFiles'
-#rootDir = r'C:\Users\E151509\Downloads\demo'
+#rootDir = r'C:\Users\E151509\Downloads\demo\demo'
 #scoreboardDir = r'C:/Users/E151509/Google Drive/Course Materials/Introduction to Computer Science/10.Python/scoreboard'
 #scoreboardDirAlt = r'C:/Users/E151509/My Drive/Course Materials/Introduction to Computer Science/10.Python/scoreboard'
 scoreboardDir = r'C:\Users\E151509\My Drive\My LASA\scoreboard'
-scoreboardDirAlt = r'C:\Users\E151509\Google Drive\My LASA\scoreboard'
-mossDir = r'C:\Users\E151509\Google Drive\My LASA\misc\tools\moss plagiarize checking'
+#scoreboardDir = r'C:\Users\E151509\Downloads\demo\demo\scoreboard_for_demo'
+mossDir = r'C:\Users\E151509\My Drive\My LASA\misc\tools\moss plagiarize checking'
 
 # email (email login is in login.py)
-emailSignature = "\n\nMr. Mueller\n"
-emailAttachmentFile = r"C:/Users/E151509/Documents/CSassignmentChecker.jpg"
+emailSignature = "\nMr. Mueller\n"
+emailAttachmentDir = r"C:/Users/E151509/Documents/"
 
 # timeout (default time in seconds for student assignment to time out - protects against endless loops
 TIMEOUT_DEFAULT = 5
@@ -34,6 +28,5 @@ TIMEOUT_DEFAULT = 5
 ##validClassPeriods = ["1","4","5"]
 ##rootDir = r'C:/Users/E151509/Downloads/demo'
 ##scoreboardDir = r'C:/Users/E151509/Downloads/demo/scoreboard'
-##scorebardDirAlt = r''
 ##emailSignature = "/nYour Teacher/n"
-##emailAttachmentFile = r"C:/Users/E151509/Downloads/CSassignmentChecker.jpg"
+##emailAttachmentDir = r"C:/Users/E151509/Downloads"
